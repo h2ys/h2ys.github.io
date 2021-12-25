f40ceaed6e7acbe06d72ad1df7f0ee502f4f4132
h2y <h2y@h2y.co.kr>
2021-12-25 14:03:34 +0900

    deploy

---
+++ .vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: "vuepress-starter",
+    description: "vuepress로 만듬",
+    themeConfig: {
+        nav: [
+            {test: "Home", link: "/"}
+        ],
+        sidebar: "auto"     // h1 ~ h6 같은 heading tag를 기준으로 sidebar를 만들어줌
+    }
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-# h2ys.github.io
-h2ys.github.io
-
-https://h2ys.github.io
